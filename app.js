b9bf11376b5ddf20a17b173bd8dd542b8e8aecf4
JustWaveThings <justwavethings@proton.me>
2023-08-30 17:50:59 -0400

    Pass passport failureMessage to be rendered

--- app.js
+++ app.js
@@ -38,13 +38,69 @@ app.set("view engine", "ejs");
 app.use(session({ secret: "dogs", resave: false, saveUninitialized: true }));
 
 // passport setup
+
+// passport local strategy   function 1
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username });
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (error) {
+      return done(error);
+    }
+  })
+);
+
+// passort serialize user function 2
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+// passport deserialize user function 3
+
+passport.deserializeUser(async function (id, done) {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (error) {
+    done(error);
+  }
+});
+
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-// get handler
-app.get("/", (req, res) => res.render("index"));
+// get handler  which gets the user from the request and renders the index page
+app.get("/", async (req, res) => {
+  let sessionAuthMessage = req.session.messages
+    ? req.session.messages.at(-1)
+    : null;
+
+  res.render("index", {
+    user: req.user,
+    authMessage: sessionAuthMessage,
+  });
+});
+
 app.get("/sign-up", (req, res) => res.render("sign-up-form"));
+
+app.get("/log-out", (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
 
 // post handler for sign-up
 app.post("/sign-up", async (req, res, next) => {
@@ -59,6 +115,17 @@ app.post("/sign-up", async (req, res, next) => {
     next(error);
   }
 });
+
+// post handler for login
+
+app.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/",
+    failureMessage: true,
+  })
+);
 
 // express server listener
 app.listen(3001, () => console.log("App listening on port 3001!"));
